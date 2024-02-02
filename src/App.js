3c8082f411a141ac4e9aba470bf254558d268f35
sreerejm7 <sreerejsree7@gmail.com>
2024-02-02 11:40:45 +0530

    adding to git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Background from './Background/Background'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Background/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Background/Background.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Background.css'
+
+function Background() {
+  return (
+    <div className='background'>
+        <div className='header'>
+        <h1 className='Title'> Epic Games : An American video game and software developer </h1>
+        <h1 className='title'>and publisher based in Cary, North Carolina.</h1>
+        </div>
+        <div className="image">
+            <img src="" alt="image1" className="image1" />
+        </div>
+        <div className='button'>
+            <button className='view'>Visit Website</button>
+        </div>
+    </div>
+
+  )
+}
+
+export default Background
